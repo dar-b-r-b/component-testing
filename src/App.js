a3e58fc3cd98d5ea3f167ac2ab2a42692afcc0b7
dar-b-r-b <d.tausheva9@gmail.com>
2024-08-20 09:55:00 +0300

    component testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("Stranger");
+  let inputName = "start";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 id="message">Hi, {name.length !== 0 ? name : "Stranger"}</h1>
+      <label for="name">Enter your name</label>
+      <input
+        type="text"
+        id="name"
+        onChange={(e) => (inputName = e.target.value)}
+      ></input>
+      <button onClick={() => setName(inputName)}>Introduce</button>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders hello message", () => {
+  render(<App name="Stranger" />);
+
+  act(() => {
+    const newLocal = screen.getByLabelText("Enter your name");
+    userEvent.type(newLocal, "Darya");
+
+    const button = screen.getByText("Introduce");
+    userEvent.click(button);
+    console.log("LOG" + button);
+  });
+
+  const helloMessage = screen.getByText("Hi, Darya");
+  expect(helloMessage).toBeInTheDocument();
 });
